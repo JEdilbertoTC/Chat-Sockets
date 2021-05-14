bf95baafe4ed2c6a597c4209514fbc5e0374368f
JOSE EDILBERTO TORRES CRUZ <A257304@alumnos.uaslp.mx>
2021-05-14 16:21:48 -0500

    Add project files.

---
+++ EjemploCliente/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace EjemploCliente
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "label1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(24, 193);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(158, 35);
+            this.textBox2.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(24, 80);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(247, 95);
+            this.listBox1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(188, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(43, 35);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 270);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ EjemploCliente/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace EjemploCliente
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient clientSocket;
+        private delegate void Cambia(string mensaje);
+        private Thread threadReceiveInformation;
+        NetworkStream serverStream;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SendInformation(string text)
+        {
+            if (!this.serverStream.CanWrite) return;
+
+            byte[] outStream = Encoding.UTF8.GetBytes(text + "id"+new Random().Next(10000000)+"$");
+            
+            this.serverStream.Write(outStream, 0, outStream.Length);
+            this.serverStream.Flush();
+            UpdateTextBox("");
+        }
+
+        private void ReceiveInformation()
+        {
+            if (!serverStream.CanRead) return;
+
+            byte[] inStream = new byte[1000000];
+            this.serverStream.Read(inStream, 0, clientSocket.ReceiveBufferSize);
+            string returndata = Encoding.UTF8.GetString(inStream);
+            returndata = returndata.Substring(0, returndata.IndexOf("$"));
+            msg(returndata);
+        }
+
+        private void UpdateTextBox(string text)
+        {
+            textBox2.Text = text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "") return;
+            SendInformation(textBox2.Text);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                label1.Text = "Client Started";
+                CreateConnection();
+                label2.Text = "Client Socket Program - Server Connected ...";
+            }
+            catch (Exception ex) { listBox1.Items.Add(ex); };
+        }
+
+        private void CreateConnection()
+        {
+            this.clientSocket = new TcpClient();
+            this.clientSocket.Connect("127.0.0.1", 8888);
+        }
+
+        public void msg(string mesg)
+        {
+            if (listBox1.InvokeRequired)
+            {
+                Cambia cambia = new Cambia(AddMessage);
+                Invoke(cambia, new object[] { mesg });
+            }
+        }
+
+        public void AddMessage(string mensaje)
+        {
+            listBox1.Items.Add(mensaje);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.serverStream = clientSocket.GetStream();
+            if (!this.serverStream.DataAvailable) return;
+
+            this.threadReceiveInformation = new Thread(ReceiveInformation);
+            this.threadReceiveInformation.SetApartmentState(ApartmentState.STA);
+            this.threadReceiveInformation.Start();
+        }
+
+    }
+}
